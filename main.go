3dfcb24ec46a7a44ec5983297a2e4d4303e604ac
Drake Delozier <drakedeloz@gmail.com>
2025-02-15 16:12:22 -0600

    repl working. help and exit commands implemented

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := cleanInput(scanner.Text())
+		c, ok := commands[userInput[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		c.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Show all commands",
+			callback:    help,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
 
 func cleanInput(text string) []string {
@@ -15,4 +50,20 @@ func cleanInput(text string) []string {
 	splitText := strings.Fields(parsedText)
 
 	return splitText
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func help() error {
+	commands := getCommands()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n\n")
+	for _, c := range commands {
+		fmt.Printf("%s: %s\n", c.name, c.description)
+	}
+	return nil
 }
